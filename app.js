6adabfdafa110b8f981c742596ee873d51131abc
Akshay Jadhav <64743494+12Akii12@users.noreply.github.com>
2021-11-26 18:03:54 +0530

    Solution of Mars Rover

--- app.js
+++ app.js
@@ -1,83 +1,98 @@
 var arguments1 = process.argv;
-// const sum = arguments1.reduce((sum, arg) => sum + parseInt(arg), 0);
-// console.log("sum of two numbers", sum);
-// const mul = arguments1.reduce((mul, arg) => mul * parseInt(arg), 1);
-// console.log("mul of two numbers", mul);
 //mars-rover problem
-var posVal = arguments1.slice(2, 4);
-var roverDir = arguments1.slice(6, 7);
-var rover = {
-    dir: roverDir[0],
-    pos: { x: posVal[0], y: posVal[1] }
+var ROVERDIR;
+(function (ROVERDIR) {
+    ROVERDIR[ROVERDIR["N"] = 1] = "N";
+    ROVERDIR[ROVERDIR["E"] = 2] = "E";
+    ROVERDIR[ROVERDIR["S"] = 3] = "S";
+    ROVERDIR[ROVERDIR["W"] = 4] = "W";
+})(ROVERDIR || (ROVERDIR = {}));
+var x = 0;
+var y = 0;
+var roverFacing = ROVERDIR.N;
+var roverDefaultPosition = function (xPos, yPos, roverFacingDir) {
+    x = xPos;
+    y = yPos;
+    roverFacing = roverFacingDir;
 };
-console.log("input", rover.pos);
-var ROVER;
-(function (ROVER) {
-    ROVER["L"] = "L";
-    ROVER["R"] = "R";
-    ROVER["M"] = "M";
-})(ROVER || (ROVER = {}));
-var roverTurnLeft = function (rover) {
-    switch (rover.dir) {
-        case "N":
-            rover.dir = "W";
-            break;
-        case "W":
-            rover.dir = "S";
-            break;
-        case "S":
-            rover.dir = "E";
-            break;
-        case "E":
-            rover.dir = "N";
-            break;
+var roverPositionDirction = function () {
+    var dir = "N";
+    if (roverFacing == 1) {
+        dir = "N";
     }
-    return console.log("rover current direction ".concat(rover.dir));
+    else if (roverFacing == 2) {
+        dir = "E";
+    }
+    else if (roverFacing == 3) {
+        dir = "S";
+    }
+    else if (roverFacing == 4) {
+        dir = "W";
+    }
+    console.log(x + " " + y + " " + dir + " ");
 };
-var roverTurnRight = function (rover) {
-    switch (rover.dir) {
-        case "N":
-            rover.dir = "E";
-            break;
-        case "E":
-            rover.dir = "S";
-            break;
-        case "S":
-            rover.dir = "W";
-            break;
-        case "W":
-            rover.dir = "N";
-            break;
-    }
-    return console.log("rover current direction ".concat(rover.dir));
-};
-var moveRover = function (roverMove) {
-    var posX = roverMove.pos.x;
-    var posY = roverMove.pos.y;
-    var moveDir = roverMove.dir;
-    if ((moveDir === "N" && posY <= 0) ||
-        (moveDir === "E" && posX >= 5) ||
-        (moveDir === "S" && posY >= 5) ||
-        (moveDir === "W" && posX <= 0)) {
-        return console.log("rover position");
-    }
-    else if (moveDir === "N" && posY <= 5) {
-        posY = posY - 1;
-        return console.log("Rover position x :".concat(posX, " and y :").concat(posY, " Rover direction: ").concat(moveDir));
-    }
-    else if (moveDir === "E" && posX < 5) {
-        posX = posX + 1;
-        return console.log("rover position x :".concat(posX, " and y :").concat(posY, " Rover direction: ").concat(moveDir));
-    }
-    else if (moveDir === "S" && posY < 5) {
-        posY = posY + 1;
-        return console.log("rover position x :".concat(posX, " and y :").concat(posY, " Rover direction: ").concat(moveDir));
-    }
-    else if (moveDir === "W" && posX < 0) {
-        posX = posX - 1;
-        return console.log("rover position x :".concat(posX, " and y :").concat(posY, " rover direction: ").concat(moveDir));
+var roverPossibleMove = function (letters) {
+    for (var i = 0; i < letters.length; i++) {
+        roverMoments(letters.charAt(i));
     }
 };
-console.log(roverTurnLeft(rover));
-console.log(roverTurnRight(rover));
-console.log(moveRover(rover));
+var roverMoments = function (letter) {
+    if (letter == "L") {
+        turnLeft();
+    }
+    else if (letter == "R") {
+        turnRight();
+    }
+    else if (letter == "M") {
+        moveRover();
+    }
+};
+var turnLeft = function () {
+    if (roverFacing == ROVERDIR.N) {
+        roverFacing = ROVERDIR.W;
+    }
+    else if (roverFacing == ROVERDIR.W) {
+        roverFacing = ROVERDIR.S;
+    }
+    else if (roverFacing == ROVERDIR.S) {
+        roverFacing = ROVERDIR.E;
+    }
+    else if (roverFacing == ROVERDIR.E) {
+        roverFacing = ROVERDIR.N;
+    }
+};
+var turnRight = function () {
+    if (roverFacing == ROVERDIR.N) {
+        roverFacing = ROVERDIR.E;
+    }
+    else if (roverFacing == ROVERDIR.E) {
+        roverFacing = ROVERDIR.S;
+    }
+    else if (roverFacing == ROVERDIR.S) {
+        roverFacing = ROVERDIR.W;
+    }
+    else if (roverFacing == ROVERDIR.W) {
+        roverFacing = ROVERDIR.N;
+    }
+};
+var moveRover = function () {
+    if (roverFacing == ROVERDIR.N) {
+        y = y + 1;
+    }
+    else if (roverFacing == ROVERDIR.E) {
+        x = x + 1;
+    }
+    else if (roverFacing == ROVERDIR.S) {
+        y = y - 1;
+    }
+    else if (roverFacing == ROVERDIR.W) {
+        x = x - 1;
+    }
+    console.log("x y dir values", x + " " + y + " " + roverFacing);
+};
+roverDefaultPosition(1, 2, ROVERDIR.N);
+roverPossibleMove("LMLMLMLMM");
+roverPositionDirction();
+roverDefaultPosition(3, 3, ROVERDIR.E);
+roverPossibleMove("MMRMMRMRRM");
+roverPositionDirction();
--- app.ts
+++ app.ts
@@ -1,93 +1,95 @@
 const arguments1 = process.argv;
-// const sum = arguments1.reduce((sum, arg) => sum + parseInt(arg), 0);
-
-// console.log("sum of two numbers", sum);
-
-// const mul = arguments1.reduce((mul, arg) => mul * parseInt(arg), 1);
-
-// console.log("mul of two numbers", mul);
 
 //mars-rover problem
-
-let posVal = arguments1.slice(2, 4);
-let roverDir = arguments1.slice(6, 7);
-
-var rover = {
-  dir: roverDir[0],
-  pos: { x: posVal[0], y: posVal[1] },
-};
-
-console.log("input", rover.pos);
-
-enum ROVER {
-  L = "L",
-  R = "R",
-  M = "M",
+enum ROVERDIR {
+  N = 1,
+  E = 2,
+  S = 3,
+  W = 4,
 }
 
-const roverTurnLeft = (rover: any) => {
-  switch (rover.dir) {
-    case "N":
-      rover.dir = "W";
-      break;
-    case "W":
-      rover.dir = "S";
-      break;
-    case "S":
-      rover.dir = "E";
-      break;
-    case "E":
-      rover.dir = "N";
-      break;
-  }
-  return console.log(`rover current direction ${rover.dir}`);
+let x = 0;
+let y = 0;
+let roverFacing = ROVERDIR.N;
+
+const roverDefaultPosition = (xPos: number, yPos: number, roverFacingDir: number) => {
+  x = xPos;
+  y = yPos;
+  roverFacing = roverFacingDir;
 };
 
-const roverTurnRight = (rover: any) => {
-  switch (rover.dir) {
-    case "N":
-      rover.dir = "E";
-      break;
-    case "E":
-      rover.dir = "S";
-      break;
-    case "S":
-      rover.dir = "W";
-      break;
-    case "W":
-      rover.dir = "N";
-      break;
+const roverPositionDirction = () => {
+  var dir = "N";
+  if (roverFacing == 1) {
+    dir = "N";
+  } else if (roverFacing == 2) {
+    dir = "E";
+  } else if (roverFacing == 3) {
+    dir = "S";
+  } else if (roverFacing == 4) {
+    dir = "W";
   }
-  return console.log(`rover current direction ${rover.dir}`);
+  console.log(x + " " + y + " " + dir + " ");
 };
 
-const moveRover = (roverMove: any) => {
-  let posX = roverMove.pos.x;
-  let posY = roverMove.pos.y;
-  let moveDir = roverMove.dir;
-
-  if (
-    (moveDir === "N" && posY <= 0) ||
-    (moveDir === "E" && posX >= 5) ||
-    (moveDir === "S" && posY >= 5) ||
-    (moveDir === "W" && posX <= 0)
-  ) {
-    return console.log("rover position");
-  } else if (moveDir === "N" && posY <= 5) {
-    posY = posY - 1;
-    return console.log(`Rover position x :${posX} and y :${posY} Rover direction: ${moveDir}`);
-  } else if (moveDir === "E" && posX < 5) {
-    posX = posX + 1;
-    return console.log(`rover position x :${posX} and y :${posY} Rover direction: ${moveDir}`);
-  } else if (moveDir === "S" && posY < 5) {
-    posY = posY + 1;
-    return console.log(`rover position x :${posX} and y :${posY} Rover direction: ${moveDir}`);
-  } else if (moveDir === "W" && posX < 0) {
-    posX = posX - 1;
-    return console.log(`rover position x :${posX} and y :${posY} rover direction: ${moveDir}`);
+const roverPossibleMove = (letters: string) => {
+  for (let i = 0; i < letters.length; i++) {
+    roverMoments(letters.charAt(i));
   }
 };
 
-console.log(roverTurnLeft(rover));
-console.log(roverTurnRight(rover));
-console.log(moveRover(rover));
+const roverMoments = (letter: string) => {
+  if (letter == "L") {
+    turnLeft();
+  } else if (letter == "R") {
+    turnRight();
+  } else if (letter == "M") {
+    moveRover();
+  }
+};
+
+const turnLeft = () => {
+  if (roverFacing == ROVERDIR.N) {
+    roverFacing = ROVERDIR.W;
+  } else if (roverFacing == ROVERDIR.W) {
+    roverFacing = ROVERDIR.S;
+  } else if (roverFacing == ROVERDIR.S) {
+    roverFacing = ROVERDIR.E;
+  } else if (roverFacing == ROVERDIR.E) {
+    roverFacing = ROVERDIR.N;
+  }
+};
+
+const turnRight = () => {
+  if (roverFacing == ROVERDIR.N) {
+    roverFacing = ROVERDIR.E;
+  } else if (roverFacing == ROVERDIR.E) {
+    roverFacing = ROVERDIR.S;
+  } else if (roverFacing == ROVERDIR.S) {
+    roverFacing = ROVERDIR.W;
+  } else if (roverFacing == ROVERDIR.W) {
+    roverFacing = ROVERDIR.N;
+  }
+};
+
+const moveRover = () => {
+  if (roverFacing == ROVERDIR.N) {
+    y = y + 1;
+  } else if (roverFacing == ROVERDIR.E) {
+    x = x + 1;
+  } else if (roverFacing == ROVERDIR.S) {
+    y = y - 1;
+  } else if (roverFacing == ROVERDIR.W) {
+    x = x - 1;
+  }
+
+  console.log("x y dir values", x + " " + y + " " + roverFacing);
+};
+
+roverDefaultPosition(1, 2, ROVERDIR.N);
+roverPossibleMove("LMLMLMLMM");
+roverPositionDirction();
+
+roverDefaultPosition(3, 3, ROVERDIR.E);
+roverPossibleMove("MMRMMRMRRM");
+roverPositionDirction();
